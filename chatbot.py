56380020dc4400410ff034d5e4f18b91cb85ff74
@spolgar <123589124+arjavpoudel@users.noreply.github.com>
2023-02-26 17:08:13 +0000

    functionised logic to make callable
    put chatbot logic into a funtion which is then called by the facial_recognition module.

--- chatbot.py
+++ chatbot.py
@@ -1,62 +1,66 @@
 import speech_recognition as sr
 import pyttsx3
 import openai
-
-r = sr.Recognizer()
-engine = pyttsx3.init()
-engine.setProperty('rate', 190)
-openai.api_key = "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"
+from greet_me import greet_me
 
 
-engine.say("Good evening Chandra.")
-engine.runAndWait()
+def chatbot():
 
-conversation = []
-max_history = 5
-user_name = "Arjav"
-bot_name = "Turing"
+    r = sr.Recognizer()
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 180)
+    openai.api_key = "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"
 
-# Adjust microphone sensitivity once at the beginning of the program
-with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source, duration=0.4)
+    greet_me()
 
-while True:
+    conversation = []
+    max_history = 5
+    user_name = "Arjav"
+    bot_name = "Turing"
+
+    # Adjust microphone sensitivity once at the beginning of the program
     with sr.Microphone() as source:
-        print("\n Listening...")
-        audio = r.listen(source)
-    print("processing...")
+        r.adjust_for_ambient_noise(source, duration=0.4)
 
-    try:
-        user_input = r.recognize_google(audio)
-    except:
-        continue
+    while True:
+        with sr.Microphone() as source:
+            print("\n Listening...")
+            audio = r.listen(source)
+        print("processing...")
 
-    prompt = user_name + ": " + user_input + "\n" + bot_name + ": "
-    conversation.append(prompt)
+        try:
+            user_input = r.recognize_google(audio)
+        except:
+            continue
 
-    # Limit conversation to the last max_history exchanges
-    if len(conversation) > max_history:
-        conversation = conversation[-max_history:]
+        prompt = user_name + ": " + user_input + "\n" + bot_name + ": "
+        conversation.append(prompt)
 
-    # Construct input prompt from last max_history exchanges
-    prompt = "".join(conversation)
+        # Limit conversation to the last max_history exchanges
+        if len(conversation) > max_history:
+            conversation = conversation[-max_history:]
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0.8,
-        max_tokens=128,
-        top_p=0.5,
-        frequency_penalty=0,
-        presence_penalty=0
-)
+        # Construct input prompt from last max_history exchanges
+        prompt = "".join(conversation)
 
-    
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=1,
+            max_tokens=128,
+            top_p=0.5,
+            frequency_penalty=0,
+            presence_penalty=0
+    )
 
-    response_str = response["choices"][0]["text"].replace("\n", "")
-    response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
+        
 
-    conversation[-1] += response_str + "\n"
+        response_str = response["choices"][0]["text"].replace("\n", "")
+        response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
 
-    engine.say(response_str)
-    engine.runAndWait()
+        conversation[-1] += response_str + "\n"
+
+        engine.say(response_str)
+        engine.runAndWait()
+
+
