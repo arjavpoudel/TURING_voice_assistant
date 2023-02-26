64e49e6001b835c7bfd6342de621d9fccc5ada6c
@spolgar <123589124+arjavpoudel@users.noreply.github.com>
2023-02-26 17:53:45 +0000

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -10,24 +10,23 @@ engine =pyttsx3.init()
 def get_time():
     # Get current time
     now = datetime.datetime.now()
-    # Determine time of day
     if now.hour < 12:
-        engine.say("Good morning Aswin")
+        engine.say("Good morning {name_here}")
     elif now.hour < 18:
-        engine.say("Good afternoon Aswin")
+        engine.say("Good afternoon {name_here}")
     elif now.hour < 22:
-        engine.say("Good evening")
+        engine.say("Good evening {name_here}")
     else:
-        engine.say("Good night")
+        engine.say("Good night {name_here}")
 
 
 
 
-
+#Use a site like WeatherAPI.com for free api key
 def get_weather():
      # Get weather data
-    api_key = "8eba66a28b7d4a77b42143901232602"
-    city_name = 'Dundee'
+    api_key = "your-api-key-here"
+    city_name = '{your-city-here}'
     url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city_name}&aqi=no'
     response = requests.get(url)
     data = response.json()
